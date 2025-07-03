05353d69a95529b158576b0fad23fd834383428b
MhlabaDev <hlabisto28@gmail.com>
2025-07-03 09:19:37 +0000

    commit

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,25 +1,7 @@
-#!/usr/bin/python3
-import sys
-
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
         n -= 1  # Decrement n to avoid infinite loop
     return result
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./factorial.py <number>")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-        if n < 0:
-            print("Factorial is not defined for negative numbers.")
-            sys.exit(1)
-        f = factorial(n)
-        print(f)
-    except ValueError:
-        print("Please provide a valid integer.")
-        sys.exit(1)
 
---
+++ debugging/print_arguments.py
@@ -0,0 +1,5 @@
+import sys
+
+for i in range(1, len(sys.argv)):  # Start at 1 to skip script name
+    print(sys.argv[i])
+
